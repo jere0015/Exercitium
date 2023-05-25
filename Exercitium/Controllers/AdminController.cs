428fe474b1ec6106234c609f5dc5a8a8cb906e7c
jere0015 <43599281+jere0015@users.noreply.github.com>
2023-05-25 16:47:29 +0200

    #7 made admin controller for making new roles. Also added logo for navbar and started customizing profile dropdown in navbar and started creating profile view

---
+++ Exercitium/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercitium.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            if (!string.IsNullOrEmpty(roleName))
+            {
+                var roleExists = await _roleManager.RoleExistsAsync(roleName);
+                if (!roleExists)
+                {
+                    var newRole = new IdentityRole(roleName);
+                    await _roleManager.CreateAsync(newRole);
+                }
+            }
+
+            return View();
+        }
+    }
+}

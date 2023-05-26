aea81d799b1868b1402c7437c08dec81dc0804a4
jere0015 <43599281+jere0015@users.noreply.github.com>
2023-05-26 12:08:54 +0200

    #16 line chart works. It's done

---
+++ Exercitium/Controllers/StatisticsController.cs
@@ -0,0 +1,73 @@
+﻿using Exercitium.Models;
+using Exercitium.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Exercitium.Data;
+
+namespace Exercitium.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ExercitiumContext _context;
+
+        public StatisticsController(UserManager<User> userManager, ExercitiumContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var exercises = _context.Exercises.ToList();
+            var viewModel = new ExerciseStatisticsViewModel
+            {
+                Exercises = exercises,
+                SelectedExerciseId = exercises.FirstOrDefault()?.Id,
+                DefaultExerciseName = exercises.FirstOrDefault()?.ExerciseName
+            };
+            return View(viewModel);
+        }
+
+        // GET: Statistics/GetExerciseData?exerciseId={exerciseId}
+        public IActionResult GetExerciseData(int exerciseId)
+        {
+            var exercise = _context.Exercises.FirstOrDefault(e => e.Id == exerciseId);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            var userId = _userManager.GetUserId(User);
+
+            var workouts = _context.Workouts
+                .Include(w => w.WorkoutExercises)
+                .Where(w => w.UserId == userId)
+                .ToList();
+
+            var dates = new List<DateTime>();
+            var weights = new List<double>();
+
+            foreach (var workout in workouts)
+            {
+                var workoutExercise = workout.WorkoutExercises.FirstOrDefault(we => we.ExerciseId == exerciseId);
+                if (workoutExercise != null)
+                {
+                    dates.Add(workout.DateTime);
+                    weights.Add(workoutExercise.Weight);
+                }
+            }
+
+            var sortedData = dates.Zip(weights, (d, w) => new { Date = d, Weight = w })
+                          .OrderBy(d => d.Date)
+                          .ToList();
+
+            dates = sortedData.Select(d => d.Date).ToList();
+            weights = sortedData.Select(d => d.Weight).ToList();
+
+            var data = new { dates, weights };
+            return Json(data);
+        }
+
+    }
+}
